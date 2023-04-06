2a063cb1b09d3b0f1a29a774ab8a3cf473b92a7d
myki3 Michael <>
2023-04-06 21:36:15 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import random
+
+random_int = random.randint(1, 50)
+
+print(random_int)
+
+random_float = random.random() * 50
+
+print(random_float)
+
+love_score = random.randint(1, 100)
+
+print(f"Your love_score is {love_score}.")
